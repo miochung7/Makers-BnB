bb3450ae3e67b7c35166e46d14c9f0eb057b0d84
Elena <elenaevans10@gmail.com>
2021-01-20 16:48:02 +0000

    attempting to add flash alerts, work in progress

--- lib/users.rb
+++ lib/users.rb
@@ -1,6 +1,7 @@
 require 'pg'
 require_relative 'database_connection'
 require_relative 'database_connection_setup'
+require './helpers/helpers'
 
 class User
 
@@ -8,7 +9,23 @@ class User
     result = DatabaseConnection.query('SELECT * FROM users')
     result.map { |user| user['email_address']}
   end
+  
   def self.sign_up(email_address, password)
     DatabaseConnection.query("INSERT INTO users (email_address, password) VALUES('#{email_address}', '#{password}')")
+    
   end
+
+  def self.log_in(email_address, password)
+    if user_exists?(email_address)
+      entered_password = DatabaseConnection.query("SELECT password FROM users WHERE email_address = '#{email_address}'")
+      if entered_password == password
+        #login
+      else
+        return "Your details are incorrect"
+      end
+    else 
+      return "Your details are incorrect"
+    end
+  end
+  
 end
